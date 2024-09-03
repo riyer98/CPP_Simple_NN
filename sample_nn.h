0521f2d0e493f4c4b19c4e90fa4a0196ddb61b0c
riyer98 <89757136+riyer98@users.noreply.github.com>
2024-09-03 14:29:13 +0530

    Update sample_nn.h

--- sample_nn.h
+++ sample_nn.h
@@ -11,9 +11,12 @@ class NeuralNet {
     int input_size;
     int output_size;
 
+    protected:
     vector<vector<vector<float>>> weights;
     vector<vector<float>> layers;
 
+    float activation(float z);
+    vector<float> final_activation (vector<float> &finlayer);
 
     public:
     NeuralNet(int output_size);
@@ -22,8 +25,5 @@ class NeuralNet {
     void getParams(string filename);
 
     vector<float> getOutput(vector<float> &input_vec);
-
-    float activation(float z);
-    vector<float> final_activation (vector<float> &finlayer);
     
 };
